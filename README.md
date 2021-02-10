dda6e1fa738236f8c3b2d922f5839ffc3032df22
Peter Squicciarini <stripedpajamas273@gmail.com>
2021-02-10 13:19:16 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# ilp-payout-sender
--- index.js
+++ index.js
@@ -3,28 +3,23 @@ const SPSP = require('ilp-protocol-spsp')
 const Plugin = require('ilp-plugin-btp')
 
 exports.handler = async (event) => {
+  console.log('here event received', { event })
+  const {
+    destination_account: destinationAccount,
+    shared_secret: sharedSecret
+  } = await SPSP.query('$ilp.uphold.com/yn8MbK2wan7P')
 
-  console.log('here event received')
-  async function main () {
-    const {
-      destination_account: destinationAccount,
-      shared_secret: sharedSecret
-    } = await SPSP.query('$ilp.uphold.com/yn8MbK2wan7P')
-  
-    console.error({ destinationAccount, sharedSecret })
-  
-    const connection = await createConnection({
-      plugin: new Plugin({ server: 'btp+ws://:asdf@ilp.flossbank.io:7768' }),
-      destinationAccount,
-      sharedSecret: Buffer.from(sharedSecret, 'base64')
-    })
-  
-    console.log('connection created')
-    const stream = connection.createStream()
-    console.log('stream created')
-    stream.setSendMax(150)
-    console.log('set max ')
-  }
-  
-  main().catch((e) => console.log(e))
+  console.error({ destinationAccount, sharedSecret })
+
+  const connection = await createConnection({
+    plugin: new Plugin({ server: 'btp+ws://:asdf@ilp.flossbank.io:7768' }),
+    destinationAccount,
+    sharedSecret: Buffer.from(sharedSecret, 'base64')
+  })
+
+  console.log('connection created')
+  const stream = connection.createStream()
+  console.log('stream created')
+  stream.setSendMax(150)
+  console.log('set max ')
 }
