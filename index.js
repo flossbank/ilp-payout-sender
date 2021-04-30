7b7a1c9b9234faba0bdd39fe54c2911a886bf194
Joel Wasserman <joel.wasserman8@gmail.com>
2021-04-30 13:56:48 -0700

    refactor payout sender to hook up to mongo and dynamo

--- index.js
+++ index.js
@@ -1,86 +1,33 @@
-const { createConnection } = require('ilp-protocol-stream')
-const SPSP = require('ilp-protocol-spsp')
-const Plugin = require('ilp-plugin-btp')
+const AWS = require('aws-sdk')
+const Pino = require('pino')
+const Process = require('./lib/process')
+const Config = require('./lib/config')
+const Db = require('./lib/mongo')
+const Dynamo = require('./lib/dynamo')
+const Ilp = require('./lib/ilp')
 
-// the address of the internal NLB that the lambda will call to connect to the ILP connector
-const { ILP_CONNECTOR_ADDR } = process.env
+const kms = new AWS.KMS({ region: 'us-west-2' })
+const docs = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' })
 
-// creates stream, sets max, resolves when stream acknowledges outgoing money
-function sendMoney (connection, amount) {
-  return new Promise((resolve, reject) => {
-    const state = {
-      moneySent: false,
-      totalSent: 0
-    }
-
-    console.log('Creating stream...')
-    const stream = connection.createStream()
-    console.log('Stream created successfully')
-
-    stream.on('outgoing_money', (sentAmount) => {
-      const sentAmountInt = parseInt(sentAmount, 10)
-
-      console.log('Money sent successfully (%d)!', sentAmountInt)
-
-      state.totalSent += sentAmountInt
-
-      if (state.totalSent === amount) {
-        state.moneySent = true
-
-        console.log('All money is sent. Closing stream...')
-        stream.destroy()
-      } else {
-        console.log('%d !== %d... still waiting for all money to send', state.totalSent, amount)
-      }
-    })
-
-    stream.once('close', () => {
-      console.log('Payment stream closed')
-      if (state.moneySent) resolve()
-      else reject(new Error('Stream closed without emitting outgoing_money event'))
-    })
-
-    stream.once('error', (err) => {
-      reject(err)
-    })
-
-    console.log('Setting stream max to specified amount (%d)', amount)
-    stream.setSendMax(amount)
-  })
-}
-
+/*
+- Get maintainer id from SQS event
+- Lock on maintainer id for processing so no other lambda duplicates payout
+- Look up payouts for the maintainer that have not been paid, as well as their ILP pointer
+- Initiate ILP sending of payment
+- Mark payouts as paid, as well as the timestamp and payment pointer they were sent to
+- Unlock maintainer ID and clean up
+*/
 exports.handler = async (event) => {
-  const { walletAddress, amount } = event
+  const log = Pino()
+  const dynamo = new Dynamo({ log, docs })
+  const config = new Config({ log, kms })
+  const ilp = new Ilp({ log })
+  const db = new Db({ log, config })
+  await db.connect()
 
-  console.log('Sending %d to %s', amount, walletAddress)
-
-  const {
-    destination_account: destinationAccount,
-    shared_secret: sharedSecret
-  } = await SPSP.query(walletAddress)
-
-  console.log({ destinationAccount, sharedSecret })
-
-  console.log('Creating connection to Flossbank ILP connector...')
-  const plugin = new Plugin({ server: `btp+ws://:asdf@${ILP_CONNECTOR_ADDR}:7768` })
-
-  const connection = await createConnection({
-    plugin,
-    destinationAccount,
-    sharedSecret: Buffer.from(sharedSecret, 'base64'),
-    slippage: 1.0
-  })
-
-  console.log('Connection created successfully')
-
-  await sendMoney(connection, amount)
-
-  console.log('Closing connection to ILP connector...')
-  await connection.end()
-
-  // connection will probably disconnect the plugin automatically
-  if (plugin.isConnected()) {
-    console.log('Disconnecting from BTP server')
-    await plugin.disconnect()
+  try {
+    await Promise.all(event.Records.map(record => Process.process({ record, db, dynamo, ilp, log })))
+  } finally {
+    await db.close()
   }
 }
